fa74862230a09ef7f45f6862be2bab18b64efc33
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-16 12:35:52 +0200

    fix: remove example data from resume-content

--- src/resume-content.js
+++ src/resume-content.js
@@ -7,31 +7,6 @@ let resumeContent = {
     email: 'john.doe@gmail.com',
     phoneNo: '+49 175 ----1840',
   },
-  education: [
-    {
-      key: crypto.randomUUID(),
-      institutionName: 'TUM (Technische Universität München)',
-      degree: 'Computer Science',
-      place: 'Munich, Germany',
-      graduationDate: 'June 2024',
-    },
-  ],
-  professionalExperience: [
-    {
-      key: crypto.randomUUID(),
-      institutionName: 'Amazon',
-      role: 'Senior Developer',
-      place: 'London, UK',
-      dates: 'June 2024 - Current',
-      description: 'Created Amazon Alexa and drone delivery',
-    },
-    {
-      key: crypto.randomUUID(),
-      institutionName: 'Amazon',
-      role: 'Senior Developer',
-      place: 'London, UK',
-      dates: 'June 2024 - Current',
-      description: 'Created Amazon Alexa and drone delivery',
-    },
-  ],
+  education: [],
+  professionalExperience: [],
 };
